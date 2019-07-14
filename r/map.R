778498dfa4980c17702ef3cf2926a526bf9a4b7c
Jason Kao <jason.kao@columbia.edu>
2019-07-14 19:01:10 -0400

    getting there

---
+++ r/map.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(jsonlite)
+library(maps)
+library(mapdata)
+
+events <- fromJSON("../data/events.json")
+
+states <- map_data("state")
+ggplot() +
+  geom_polygon(data = states, aes(x = long, y = lat, group = group), fill = "gray80", color = "white") + 
+  geom_point(data = events, aes(x = lng, y = lat)) +
+  coord_fixed(1.3)
+  
